5beec1ed6cc7174c01fc8fad5fcefe304a61108a
Yuliya <yuliya.sm7@gmail.com>
2020-04-06 22:21:58 +0300

    local streaming work

--- livecam.js
+++ livecam.js
@@ -1,5 +1,12 @@
 const LiveCam = require('livecam');
 const webcam_server = new LiveCam({
+    'ui_addr' : '192.168.1.105',
+    'ui_port' : 8081,
+    'broadcast_addr' : '192.168.1.105',
+    'broadcast_port' : 12000,
+    'gst_tcp_addr' : '192.168.1.105',
+    'gst_tcp_port' : 10000,
+    
     'start' : function() {
         console.log('WebCam server started!');
     }

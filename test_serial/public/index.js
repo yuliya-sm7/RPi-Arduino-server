fb6d4fcfb83d268db5fd4d2096fa812ea199dced
yuliya <yuliya.sm7@gmail.com>
2020-05-28 21:06:12 +0000

    work with Robot

--- test_serial/public/index.js
+++ test_serial/public/index.js
@@ -1,33 +1,79 @@
 const info = document.getElementById('info');
 
-const turn = document.getElementById("customSwitch3");
-turn.addEventListener('click', (e) => {
+const light = document.getElementById("customSwitch3");
+light.addEventListener('click', (e) => {
     const data = { "led": e.target.checked }
-
     fetch('/led', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json;charset=utf-8' },
         body: JSON.stringify(data)
     }).then(res => res.text())
-    .then((msg) => {
-        info.value += msg + '\n';
-        info.scrollTop = info.scrollHeight;
-    });
+        .then(msg => logg(msg));
 });
 
+const Arduino = document.getElementById('customSwitch1');
+Arduino.addEventListener('click', (e) => {
+    const data = { "serial": e.target.checked }
+    fetch('/init', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json;charset=utf-8' },
+        body: JSON.stringify(data)
+    }).then(res => res.text())
+        .then(msg => logg(msg));
+});
+
+let video = document.getElementById("video");
+const camera = document.getElementById('customSwitch2');
+camera.addEventListener('click', (e) => {
+    if (e.target.checked) {
+        video.src = "http://37.110.86.243:8081/";
+        video.alt = "Ошибка камеры!";
+    } else {
+        video.src = "#";
+        video.alt = "Камера отключена!";
+    }
+});
 
 const form = document.forms.namedItem('toArduino');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const data = { 'command': form.command.value };
+    sendCommand(form.command.value);
+    form.reset(); // очищаем поля формы 
+});
+
+function logg(msg) {
+    info.value += msg + '\n';
+    info.scrollTop = info.scrollHeight;
+}
+
+
+document.getElementById('joystick').addEventListener('click', (e) => {
+    switch (e.target.id) {
+        case 'forward':
+            sendCommand("SetMotor 100 100 100\n");
+            break;
+        case 'left':
+            sendCommand("SetMotor -200 200 100\n");
+            break;
+        case 'right':
+            sendCommand("SetMotor 200 -200 100\n");
+            break;
+        case 'backward':
+            sendCommand("SetMotor -100 -100 100\n");
+            break;
+        case 'stop':
+            sendCommand("SetMotor 1 1 1\n");
+            break;
+    };
+});
+
+
+function sendCommand(str) {
+    const data = { 'command': str };
     fetch('/arduino', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json;charset=utf-8' },
         body: JSON.stringify(data)
     }).then(res => res.text())
-        .then((msg) => {
-            info.value += msg + '\n';
-            info.scrollTop = info.scrollHeight;
-        });
-    form.reset(); // очищаем поля формы 
-});
+        .then(msg => logg(msg));
+}
--- test_serial/test.js
+++ test_serial/test.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const SerialPort = require("serialport");
+const gpiop = require('rpi-gpio').promise;
+
+
+
+//GPIO
+gpiop.setup(37, gpiop.DIR_OUT)
+gpiop.setup(40, gpiop.DIR_OUT)
 
 
 // SERIAL
@@ -14,30 +21,35 @@ const sp = new SerialPort(portName, {
     flowControl: false,
     autoOpen: false
 });
-sp.on('error', err => { console.log(err.message); })
-sp.on('open', function () {
-    const s = "comm port ready\r";
-    sp.write(s);
-    console.log(s);
+sp.on('error', err => console.log(err.message))
+sp.on('open', () => {
+	console.log("COM port ready")
+	sp.write( "TestMode On\n")
 });
+sp.on('data', msg => console.log("From serial: " + msg))
+sp.on('close', () => console.log("COM port close"));
+sp.open();
 
-sp.on('data', msg => console.log(msg + ' from serial'))
-sp.on('close', function () { console.log("comm port close"); });
 
 //HTTP SERVER
 const port = 3141;
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.listen(port);
 app.post('/led', function (req, res) {
     const turn = req.body.led;
-    console.log(turn)
-    res.send(turn?"ВКЛ":"ВЫКЛ")
+    gpiop.write(37, turn);
+    gpiop.write(40, turn);
+    res.send(turn ? "ВКЛ" : "ВЫКЛ")
+});
+app.post('/init', function (req, res) {
+    const init = req.body.serial;
+    init ? sp.open() : sp.close();
+    res.send("Serial is " + (init ? "open" : "close"))
 });
 app.post('/arduino', function (req, res) {
     const command = req.body.command;
-    console.log(command);
     sp.write(command + '\n');
     res.send(command + ' send to Arduino')
 });
